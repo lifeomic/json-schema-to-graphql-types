326235965d7d33f7832987d10c1f17aec7f0a50e
MorganHuegel <mhue2910@gmail.com>
2018-11-06 20:19:31 -0500

    changed error-handling functions to use async/await syntax

--- src/error-handling.js
+++ src/error-handling.js
@@ -2,48 +2,46 @@ const path = require('path');
 const fs = require('fs-extra');
 
 // Directory-name must exist, and Directory-name must point to valid directory
-function validatePathName (dir) {
-  return fs.readdir(dir)
-    .then(function (res) {
-      return Promise.resolve(res);
-    })
-    .catch(function (err) {
-      if (err.name === 'TypeError [ERR_INVALID_ARG_TYPE]') {
-        err.subMessage = `Must include a directory name in the command 'convert-json-schemas-to-graphql-types <directory-name>'`;
-      } else if (err.errno === -2) {
-        err.subMessage = `The path name "${err.path}" is not a valid directory`;
-      }
+async function validatePathName (dir) {
+  try {
+    const files = await fs.readdir(dir);
+    return files;
+  } catch (err) {
+    if (err.name === 'TypeError [ERR_INVALID_ARG_TYPE]') {
+      err.subMessage = `Must include a directory name in the command 'convert-json-schemas-to-graphql-types <directory-name>'`;
+    } else if (err.errno === -2) {
+      err.subMessage = `The path name "${err.path}" is not a valid directory`;
+    }
 
-      throw err;
-    });
+    throw err;
+  }
 }
 
 // Each file must have .json extension, and each file must be syntactically correct, and no file is an array of schema
-function validateJSONSyntax (file, dir) {
+async function validateJSONSyntax (file, dir) {
   if (path.extname(file) !== '.json') {
     const err = new TypeError(`All files in directory must have .json extension`);
     err.subLocation = `${dir + file}`;
     throw err;
   }
 
-  return fs.readFile(path.join(dir, file))
-    .then(function (fileContent) {
-      const parsedFileContent = JSON.parse(fileContent);
-      if (JSON.stringify(parsedFileContent).startsWith('[')) {
-        const err = new TypeError(`File '${file}' contents cannot start with '[' character`);
-        err.subMessage = `Each file must only include only one json-schema, not an array of schema`;
-        err.subLocation = `${dir + file}`;
-        throw err;
-      }
-
-      return Promise.resolve(parsedFileContent);
-    })
-    .catch(function (err) {
-      if (err.subMessage) throw err; // Specific error from above
-      err.subMessage = `Invalid JSON syntax in file '${file}'`;
+  try {
+    const fileContent = await fs.readFile(path.join(dir, file));
+    const parsedFileContent = JSON.parse(fileContent);
+    if (JSON.stringify(parsedFileContent).startsWith('[')) {
+      const err = new TypeError(`File '${file}' contents cannot start with '[' character`);
+      err.subMessage = `Each file must only include only one json-schema, not an array of schema`;
       err.subLocation = `${dir + file}`;
       throw err;
-    });
+    }
+
+    return parsedFileContent;
+  } catch (err) {
+    if (err.subMessage) throw err; // Specific error from above
+    err.subMessage = `Invalid JSON syntax in file '${file}'`;
+    err.subLocation = `${dir + file}`;
+    throw err;
+  }
 }
 
 // Schema must contain an id or $id key to give it a graphQL type name, and top-level schema must be object type
