88ec6360babd5ed2face533e2975b6bee5226fe2
MorganHuegel <mhue2910@gmail.com>
2018-11-08 09:17:28 -0500

    updated some tests to test directory names with and without ending "/"

--- test/error-handling.js
+++ test/error-handling.js
@@ -36,44 +36,66 @@ test('should return json data from .json file', async function (test) {
   test.deepEqual(file.properties, dummyData.properties);
 });
 
-test('throws error if files are not all .json', function (test) {
-  ['./src', './src/'].forEach(async function (directory) {
-    try {
-      await validators.validateJSONSyntax('converter.js', directory);
-      test.fail('Should throw error');
-    } catch (err) {
-      test.is(err.message, `All files in directory must have .json extension`);
-      test.is(err.subLocation, `./src/converter.js`);
-    }
-  });
+test('throws error if files are not all .json', async function (test) {
+  const directory = './src';
+  try {
+    await validators.validateJSONSyntax('converter.js', directory);
+    test.fail('Should throw error');
+  } catch (err) {
+    test.is(err.message, `All files in directory must have .json extension`);
+    test.is(err.subLocation, `./src/converter.js`);
+  }
+
+  try {
+    await validators.validateJSONSyntax('converter.js', `${directory}/`);
+    test.fail('Should throw error');
+  } catch (err) {
+    test.is(err.message, `All files in directory must have .json extension`);
+    test.is(err.subLocation, `./src/converter.js`);
+  }
 });
 
-test('throws error if file contains an array of schema', function (test) {
+test('throws error if file contains an array of schema', async function (test) {
   const file = 'fail-1.json';
-  ['./test/dummy-data', './test/dummy-data/'].forEach(async function (directory) {
-    try {
-      await validators.validateJSONSyntax(file, directory);
-      test.fail('Should throw error');
-    } catch (err) {
-      test.is(err.message, `File '${file}' contents cannot start with '[' character`);
-      test.is(err.subMessage, `Each file must only include only one json-schema, not an array of schema`);
-      test.is(err.subLocation, `./test/dummy-data/${file}`);
-    }
-  });
+  const directory = './test/dummy-data';
+  try {
+    await validators.validateJSONSyntax(file, directory);
+    test.fail('Should throw error');
+  } catch (err) {
+    test.is(err.message, `File '${file}' contents cannot start with '[' character`);
+    test.is(err.subMessage, `Each file must only include only one json-schema, not an array of schema`);
+    test.is(err.subLocation, `./test/dummy-data/${file}`);
+  }
+
+  try {
+    await validators.validateJSONSyntax(file, `${directory}/`);
+    test.fail('Should throw error');
+  } catch (err) {
+    test.is(err.message, `File '${file}' contents cannot start with '[' character`);
+    test.is(err.subMessage, `Each file must only include only one json-schema, not an array of schema`);
+    test.is(err.subLocation, `./test/dummy-data/${file}`);
+  }
 });
 
-test('throws error if json syntax is incorrect', function (test) {
+test('throws error if json syntax is incorrect', async function (test) {
   const file = 'fail-2.json';
-  ['./test/dummy-data', './test/dummy-data/'].forEach(async function (directory) {
-    try {
-      await validators.validateJSONSyntax(file, directory);
-      test.fail('Should throw error');
-    } catch (err) {
-      test.is(err.message.startsWith('Unexpected token'), true);
-      test.is(err.subMessage, `Invalid JSON syntax in file '${file}'`);
-      test.is(err.subLocation, `'./test/dummy-data/${file}`);
-    }
-  });
+  try {
+    await validators.validateJSONSyntax(file, './test/dummy-data');
+    test.fail('Should throw error');
+  } catch (err) {
+    test.is(err.message.startsWith('Unexpected token'), true);
+    test.is(err.subMessage, `Invalid JSON syntax in file '${file}'`);
+    test.is(err.subLocation, `./test/dummy-data/${file}`);
+  }
+
+  try {
+    await validators.validateJSONSyntax(file, './test/dummy-data/');
+    test.fail('Should throw error');
+  } catch (err) {
+    test.is(err.message.startsWith('Unexpected token'), true);
+    test.is(err.subMessage, `Invalid JSON syntax in file '${file}'`);
+    test.is(err.subLocation, `./test/dummy-data/${file}`);
+  }
 });
 
 test('throws error if typeName is not defined', function (test) {
